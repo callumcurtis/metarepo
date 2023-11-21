962f2bf62d3e66236fae2756ff466cf2634073e3
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-11-21 06:35:32 +0000

    chore: keep standard project structure variables after activation

--- tools/lib/standard_project_structure.sh
+++ tools/lib/standard_project_structure.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
 #
-# Provide project structure information.
+# Provide standard project structure information.
 
-set_project_structure_variables() {
+set_standard_project_structure_variables() {
     PROJECT_ROOT="$(git rev-parse --show-toplevel)"
     TOOLS_DIR="${PROJECT_ROOT}/tools"
     TOOLS_HOOKS_DIR="${TOOLS_DIR}/hooks"
     TOOLS_HOOKS_ACTIVATE_DIR="${TOOLS_HOOKS_DIR}/activate"
     ENVIRONMENT_DIR="${PROJECT_ROOT}/environment"
-    ENVIRONMENT_AIRFLOW_DIR="${ENVIRONMENT_DIR}/airflow"
 }
 
-unset_project_structure_variables() {
+unset_standard_project_structure_variables() {
     unset PROJECT_ROOT
     unset TOOLS_DIR
     unset TOOLS_HOOKS_DIR
     unset TOOLS_HOOKS_ACTIVATE_DIR
     unset ENVIRONMENT_DIR
-    unset ENVIRONMENT_AIRFLOW_DIR
 }
