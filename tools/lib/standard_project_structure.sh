ae5bd21fc4ab3be4f1f2298117a849276f48771f
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-11-22 04:52:57 +0000

    refactor: rename PROJECT_ROOT to PROJECT_ROOT_DIR

--- tools/lib/standard_project_structure.sh
+++ tools/lib/standard_project_structure.sh
@@ -3,15 +3,15 @@
 # Provide standard project structure information.
 
 set_standard_project_structure_variables() {
-    PROJECT_ROOT="$(git rev-parse --show-toplevel)"
-    TOOLS_DIR="${PROJECT_ROOT}/tools"
+    PROJECT_ROOT_DIR="$(git rev-parse --show-toplevel)"
+    TOOLS_DIR="${PROJECT_ROOT_DIR}/tools"
     TOOLS_HOOKS_DIR="${TOOLS_DIR}/hooks"
     TOOLS_HOOKS_ACTIVATE_DIR="${TOOLS_HOOKS_DIR}/activate"
-    ENVIRONMENT_DIR="${PROJECT_ROOT}/environment"
+    ENVIRONMENT_DIR="${PROJECT_ROOT_DIR}/environment"
 }
 
 unset_standard_project_structure_variables() {
-    unset PROJECT_ROOT
+    unset PROJECT_ROOT_DIR
     unset TOOLS_DIR
     unset TOOLS_HOOKS_DIR
     unset TOOLS_HOOKS_ACTIVATE_DIR
