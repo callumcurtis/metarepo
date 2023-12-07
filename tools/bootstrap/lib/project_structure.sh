5fd3e887fff78a62062778dd163272e7545c8a85
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-12-07 04:41:15 +0000

    chore: only activate task in bootstrap activation script

---
+++ tools/bootstrap/lib/project_structure.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#
+# Provide project structure information for bootstrapping.
+
+set_bootstrap_project_structure_variables() {
+    __BOOTSTRAP__TASK_DIR="$(git rev-parse --show-toplevel)/.environment/task"
+}
+
+unset_bootstrap_project_structure_variables() {
+    unset __BOOTSTRAP__TASK_DIR
+}
