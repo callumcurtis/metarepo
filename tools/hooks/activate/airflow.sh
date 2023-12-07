22c063522a06d6f2e47460dbde01765bc7c78096
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-12-07 03:35:17 +0000

    chore: remove redundant shell scripts in favor of tasks

--- tools/hooks/activate/airflow.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-#
-# Activate airflow.
-
-export ENVIRONMENT_AIRFLOW_DIR="${ENVIRONMENT_DIR}/airflow"
-export AIRFLOW_HOME="${ENVIRONMENT_AIRFLOW_DIR}"
--- tools/hooks/activate/task.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-#
-# Activate Task.
-#
-# Sets the path to the Task binary.
-
-export PATH="${ENVIRONMENT_TASK_DIR}/bin:${PATH}"
