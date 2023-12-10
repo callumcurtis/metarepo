9fe90cef40579b3ede7707902816cac65f772486
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-12-10 05:29:23 +0000

    refactor: remove unnecessary double-quotes from bash commands

--- tools/bootstrap/lib/project_structure.sh
+++ tools/bootstrap/lib/project_structure.sh
@@ -3,7 +3,7 @@
 # Provide project structure information for bootstrapping.
 
 set_bootstrap_project_structure_variables() {
-    __BOOTSTRAP__TASK_DIR="$(git rev-parse --show-toplevel)/.environment/task"
+    __BOOTSTRAP__TASK_DIR=$(git rev-parse --show-toplevel)/.environment/task
 }
 
 unset_bootstrap_project_structure_variables() {
