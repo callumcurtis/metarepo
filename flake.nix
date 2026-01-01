e52d7d7b383464933b9126bc9bf576d6e6179c4e
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2025-12-31 18:08:14 -0800

    fix: refer to cookiecutter in nix store

--- flake.nix
+++ flake.nix
@@ -6,14 +6,14 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
       in
       let
         metarepo = pkgs.writeShellScriptBin "metarepo" ''
-          exec ${pkgs.python313Packages.cookiecutter}/bin/cookiecutter "$@"
+          exec ${pkgs.python313Packages.cookiecutter}/bin/cookiecutter ${./.} "$@"
         '';
       in
       {
